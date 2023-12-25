59d2ef3374baefebc7ecdc190d0e4b6ce2a11140
Djillali <zr.djillali@gmail.com>
2023-12-25 13:33:27 +0100

    Fixed random at 2023-12-25T12:33:27.744Z

--- loader.js
+++ loader.js
@@ -2,35 +2,16 @@ const { execSync } = require('child_process');
 
 function commitAndPush() {
   const now = new Date();
-  const timestamp = now.toISOString(); // Generate timestamp
-  const commitMessage = `Fixed random`;
+  const commitMessage = `Fixed random at ${now.toISOString()}`;
 
-  execSync(`echo "${commitMessage}" >> xeroxba.txt`); // Appending timestamp to a file
-
+  execSync(`echo "${commitMessage}" >> xeroxba.txt`);
   execSync('git add .');
   execSync(`git commit -m "${commitMessage}"`);
   execSync('git push origin master'); // Change 'main' to your branch name if needed
 }
 
-function scheduleDailyExecution() {
-  const now = new Date();
-  const targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 22, 0, 0); // Set target time to 13:05
+// Execute the first one immediately
+commitAndPush();
 
-  // Calculate time until the next occurrence of the target time
-  let timeUntilNextExecution = targetTime - now;
-  if (timeUntilNextExecution < 0) {
-    // If the target time has already passed today, schedule it for tomorrow
-    timeUntilNextExecution += 24 * 60 * 60 * 1000; // 24 hours in milliseconds
-  }
-
-  // Schedule the first execution
-  setTimeout(function () {
-    commitAndPush();
-
-    // Schedule subsequent executions every 24 hours
-    setInterval(commitAndPush, 24 * 60 * 60 * 1000);
-  }, timeUntilNextExecution);
-}
-
-// Start scheduling daily execution
-scheduleDailyExecution();
+// Schedule subsequent executions every minute
+setInterval(commitAndPush, 60 * 1000);
