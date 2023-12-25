90ead72cdf228085abb70b97b27103921aec771c
Djillali <zr.djillali@gmail.com>
2023-12-25 13:09:00 +0100

    Fixed at: 2023-12-25T12:09:00.004Z

---
+++ loader.js
@@ -0,0 +1,36 @@
+const { execSync } = require('child_process');
+
+function commitAndPush() {
+  const now = new Date();
+  const timestamp = now.toISOString(); // Generate timestamp
+  const commitMessage = `Fixed at: ${timestamp}`;
+
+  execSync(`echo "${commitMessage}" >> xeroxba.txt`); // Appending timestamp to a file
+
+  execSync('git add .');
+  execSync(`git commit -m "${commitMessage}"`);
+  execSync('git push origin master'); // Change 'main' to your branch name if needed
+}
+
+function scheduleDailyExecution() {
+  const now = new Date();
+  const targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 9, 0, 0); // Set target time to 13:05
+
+  // Calculate time until the next occurrence of the target time
+  let timeUntilNextExecution = targetTime - now;
+  if (timeUntilNextExecution < 0) {
+    // If the target time has already passed today, schedule it for tomorrow
+    timeUntilNextExecution += 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+  }
+
+  // Schedule the first execution
+  setTimeout(function () {
+    commitAndPush();
+
+    // Schedule subsequent executions every 24 hours
+    setInterval(commitAndPush, 24 * 60 * 60 * 1000);
+  }, timeUntilNextExecution);
+}
+
+// Start scheduling daily execution
+scheduleDailyExecution();
