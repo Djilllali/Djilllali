9e9fe37b20481700c840205e91f81556796782f6
Djillali <zr.djillali@gmail.com>
2023-12-25 13:22:00 +0100

    Fixed random

--- loader.js
+++ loader.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process');
 function commitAndPush() {
   const now = new Date();
   const timestamp = now.toISOString(); // Generate timestamp
-  const commitMessage = `Fixed at: ${timestamp}`;
+  const commitMessage = `Fixed random`;
 
   execSync(`echo "${commitMessage}" >> xeroxba.txt`); // Appending timestamp to a file
 
@@ -14,7 +14,7 @@ function commitAndPush() {
 
 function scheduleDailyExecution() {
   const now = new Date();
-  const targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 9, 0, 0); // Set target time to 13:05
+  const targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 22, 0, 0); // Set target time to 13:05
 
   // Calculate time until the next occurrence of the target time
   let timeUntilNextExecution = targetTime - now;
